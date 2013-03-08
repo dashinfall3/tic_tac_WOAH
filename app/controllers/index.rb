7ccfe8d583fdb991d2a948cbed41c8be8a22a562
DBC Apprentice <apprentice@devbootcamp.com>
2013-03-08 10:56:08 -0800

    basic controllers

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,41 @@
 get '/' do
-  # Look in app/views/index.erb
   erb :index
 end
+
+post 'login' do
+
+
+end
+
+post '/signup' do
+  player = Player.new(params)
+  session[:user_id] = player.authenticate(params[:email], params[:password])
+
+  if player.save
+    redirect '/arena'
+  else
+    erb :signup
+  end
+end
+
+post '/game/new' do
+  game = Game.new(params)
+  # in before create create all the spaces for this game
+  if game.save
+    redirect "/game/#{game.id}"
+  else
+    erb :arena
+  end
+end
+
+get '/game/:game_id' do
+  @game = Game.find(params[:game_id]) 
+  erb :game
+end
+
+get '/arena' do 
+  @games = Game.all
+  erb :arena
+end
+
+
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,12 @@
 class Game < ActiveRecord::Base
   has_many :spaces
   has_and_belongs_to_many :players
+
+  before_create :generate_spaces
+
+
+  def generate_spaces
+    
+
+  end
 end
