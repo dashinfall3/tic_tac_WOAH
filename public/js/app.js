68f27792a297639b7601f0eb280cba157444f9aa
Apprentice <apprentice@dbc09.local>
2013-03-08 15:22:39 -0800

    fix minor merge changes

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,37 @@
 $(document).ready(function(){
 
+  game = {
+    id: $('.board').attr('id');
+  }
+
+  // when player clicks on space:
+  // - ajax post to update the game
   $('.space').on("click", function(){
-    var id = $(this).attr('id'); 
-    $.post('/game/update', {player, id}).done(function(server_response){
- });
+    var space = $(this).attr('id'); 
+    $.ajax({
+      url: '/game/' +game[id]+ '/update',
+      type: 'post'
+      data: {game_id: game[id], space: space},
+      dataType: 'json'
+    }).done(function(serverResponse) {
+      $('body').append('<h3> Nice move!!!! </h3>');
+      // disable board
+    })
+  });
+
+
+
+  // check every second if it's my turn
+  //   $.ajax({
+  //     url: '/game/' +game[id]+ '/check_status',
+  //     type: 'get'
+  //     dataType: 'json'
+  //   }).done(function(serverResponse) {
+  //     if true, re-enable board
+  //   })
+  // });
+
+
+
 });
+
